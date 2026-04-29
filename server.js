e604af4f18eac5f24bd2bb951b0238773be7fb1d
PANATHULA MANOJ <manojpanathula888@gmail.com>
2026-04-29 11:27:01 +0530

    environment variable change to PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require("path");
 const { nanoid } = require("nanoid");
 const Url = require("./models/url.js");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.json());
